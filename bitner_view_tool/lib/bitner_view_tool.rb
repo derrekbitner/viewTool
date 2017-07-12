c579296bc046d2cc8d2f1f0d6c6c7cbdc87cee52
SD Davis <davis@devcamp.com>
2017-07-12 16:44:13 +0000

    initial commit

---
+++ bitner_view_tool/lib/bitner_view_tool.rb
@@ -0,0 +1,6 @@
+require "bitner_view_tool/version"
+require "bitner_view_tool/renderer"
+
+module BitnerViewTool
+  # Your code goes here...
+end
---
+++ bitner_view_tool/lib/bitner_view_tool/version.rb
@@ -0,0 +1,3 @@
+module BitnerViewTool
+  VERSION = "0.1.0"
+end
